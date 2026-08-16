6f842d68f84f34827b000347797334be26c408f4
Tanuja B <tanujabhagwat3@gmail.com>
2026-08-16 22:35:28 +0530

    Add files via upload

---
+++ main5.py
@@ -0,0 +1,5 @@
+number=int(input("enter a number: "))
+if(number%7==0):
+    print("Number is multiple of 7")
+else:
+    print("not a multiple of 7")
---
+++ main6.py
@@ -0,0 +1,12 @@
+a=500000000
+b=9009999999999
+c=352801
+d=102000000000
+if(a>b and a>c and a>d):
+    print("a is greater")
+elif(b>a and b>c and b>d):
+    print("b is greater")
+elif(c>a and c>b and c>d):
+    print("c is greater")
+else:
+    print("d is greater")
